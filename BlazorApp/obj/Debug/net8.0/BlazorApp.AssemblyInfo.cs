1a8420b657ff39ee4f9d337ccfe04b9c2de3b4f0
EllaLiu0401 <ellatong0515@gmail.com>
2024-10-08 00:54:24 +1100

    Passing props to shared components

--- BlazorApp/obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
+++ BlazorApp/obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afda80dc4cd49fd43ea3c52071edd4b28af808ef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67c79071980a1e59197ba8a0b77e300bf9e3d4b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
