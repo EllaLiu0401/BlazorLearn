3abb57b6f4323a7d86e99aa04de66396ebf6164e
EllaLiu0401 <ellatong0515@gmail.com>
2024-10-07 15:07:56 +1100

    one way data binding

--- BlazorApp/obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
+++ BlazorApp/obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ca0fbf6ce94860de046cc115061c4013937ffd7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4968b932025e78f4ae8d0b5c7676a1991dac4fb1")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
