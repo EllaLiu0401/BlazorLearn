ee8b1f579f983f01181fb1c44c3b10d4892e15b8
EllaLiu0401 <ellatong0515@gmail.com>
2024-10-09 00:33:05 +1100

    Passing parameters to route

--- BlazorApp/obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
+++ BlazorApp/obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f8ae0dd5749d01997a40ba7c3d0e2c441bdb737")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3723a15b6e8c04e2122aa969e2deee927ab9b406")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
