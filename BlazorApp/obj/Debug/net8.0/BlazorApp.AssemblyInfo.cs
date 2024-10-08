501070e55035140db9dc8aad1b6de47b4f0ec1ba
EllaLiu0401 <ellatong0515@gmail.com>
2024-10-09 07:34:00 +1100

    Query String

--- BlazorApp/obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
+++ BlazorApp/obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87c3f3b1f9183de04227d06e00a390c56024f589")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+509e6f0649c31eb5f5e1d26752fdcced887fdd6f")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
