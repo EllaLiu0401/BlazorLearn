9f8ae0dd5749d01997a40ba7c3d0e2c441bdb737
EllaLiu0401 <ellatong0515@gmail.com>
2024-10-08 22:42:49 +1100

    Cascading Parameter

--- BlazorApp/obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
+++ BlazorApp/obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9eb24eedfb20c7b3f0baca6d7f644166c1f78141")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+298a75666d57aaa37a7d242a89f687c276d0aefc")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
