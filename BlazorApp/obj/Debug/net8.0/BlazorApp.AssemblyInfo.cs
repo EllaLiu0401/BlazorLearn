61d85d3d68e26efc318ba2d8d32c566973fe2ae6
EllaLiu0401 <ellatong0515@gmail.com>
2024-10-15 23:00:13 +1100

    Confirmbox in Blazor

--- BlazorApp/obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
+++ BlazorApp/obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+501070e55035140db9dc8aad1b6de47b4f0ec1ba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3149a6dc951377c8a70640bfc33a484acd4d3639")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
