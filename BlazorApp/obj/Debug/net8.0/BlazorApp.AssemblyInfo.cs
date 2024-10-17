d17114f30a2953947edad199d9f42341575a464e
EllaLiu0401 <ellatong0515@gmail.com>
2024-10-17 21:10:13 +1100

    OnParametersSet lifecycle

--- BlazorApp/obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
+++ BlazorApp/obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ec75d04c24ddef64283bb930c0acbf2ea52dc20")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffc99ea1183e5c347128f85de45451ba5792ca16")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
